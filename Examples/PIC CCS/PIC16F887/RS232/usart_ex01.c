b4c2185a9834862115aeebc2ae06fd89a7b5f9e0
abdul-rehman-2050 <supermind2002@gmail.com>
2018-11-11 20:20:01 +0500

    usart_ex01.c file added

---
+++ Examples/PIC CCS/PIC16F887/RS232/usart_ex01.c
@@ -0,0 +1,15 @@
+#include<16F887.h>
+#device *=16
+#fuses HS,NOWDT,NOPROTECT,NOLVP
+#use delay(clock=8000000)
+#use rs232(baud=9600, xmit=PIN_C6, rcv=PIN_C7)
+
+
+void main(){
+   printf("hello world\n");
+   while(True){
+      int8 inChar = getc();
+      putc(inChar);
+   }
+
+}
