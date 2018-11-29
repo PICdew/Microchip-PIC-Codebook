58ebc2f778f898e9e6ea2a538fa44587de1e2cfc
abdul-rehman-2050 <supermind2002@gmail.com>
2018-11-29 21:32:59 +0500

    10 milli second delay is working

--- Projects/PIC CCS/Centisecond Timer/centsec_timer_v01.c
+++ Projects/PIC CCS/Centisecond Timer/centsec_timer_v01.c
@@ -6,11 +6,40 @@
 #include <LCD4Bit.c>
 #include <Timevar_Utils.c>
 
+//====================================================================
+// ISRs  Timer0_freq = MCU_freq / {4 * Prescaler * (256 - TMR0)}  
+//====================================================================
 
+volatile int32 centseconds=0;
+int8 changeFlag=0;
+
+#INT_TIMER0
+void timer0_isr(void)
+{
+   clear_interrupt(INT_TIMER0);
+   set_timer0(100);  //time is 9.98 millisecond
+   centseconds++;
+   output_toggle(PIN_B0);
+   changeFlag=1;
+}
 
 void main(){
+   setup_timer_0(RTCC_INTERNAL|RTCC_DIV_128);
+   set_timer0(100);//prescaler 8 and value 56 produce 10msec delay with 1Khz frequency
+   clear_interrupt(INT_TIMER0);
+   enable_interrupts(INT_TIMER0);
+   enable_interrupts(GLOBAL);
+   lcd_init();
    while(True){
-   
+      if(changeFlag==1){
+         Timevar mTvar;
+         changeFlag=0;
+         lcd_gotoxy(4,1);
+         printf(lcd_putc,"%08Lu",centseconds);
+         timevarFromCentiseconds(centseconds,mTvar);
+         lcd_gotoxy(4,2);
+         printf(lcd_putc,"%02u:%02u:%02u",mTvar.min,mTvar.sec,mTvar.centsec);
+         
+      }
    }
-
 }
