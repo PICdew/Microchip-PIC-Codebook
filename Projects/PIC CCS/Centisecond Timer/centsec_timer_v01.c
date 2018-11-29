b7a8490aae7c23b53e7c2096434919661ff00a6b
abdul-rehman-2050 <supermind2002@gmail.com>
2018-11-29 20:40:55 +0500

    New project to create centisecond timer with lcd file added

---
+++ Projects/PIC CCS/Centisecond Timer/centsec_timer_v01.c
@@ -0,0 +1,15 @@
+#include<16F887.h>
+#device *=16
+#fuses HS,NOWDT,NOPROTECT,NOLVP
+#use delay(clock=8000000)
+
+#include <LCD4Bit.c>
+#include <Timervar_Utils.c>
+
+
+
+void main(){
+   while(True){
+   }
+
+}
